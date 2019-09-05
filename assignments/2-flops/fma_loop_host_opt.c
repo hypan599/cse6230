2c55b224f6efb272454219e6cab6111f1bbd5544
hpan48 <hpan48@gatech.edu>
2019-09-04 23:16:52 -0400

    final version of hw2

--- assignments/2-flops/fma_loop_host_opt.c
+++ assignments/2-flops/fma_loop_host_opt.c
@@ -18,9 +18,77 @@
 void
 fma_loop_host (int N, int T, float *a, float b, float c)
 {
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < T; j++) {
+  #pragma unroll(8)
+  for (int i = 0; i < N; i+=8) {
       a[i] = a[i] * b + c;
-    }
+      a[i+1] = a[i+1] * b + c;
+      a[i+2] = a[i+2] * b + c;
+      a[i+3] = a[i+3] * b + c;
+      a[i+4] = a[i+4] * b + c;
+      a[i+5] = a[i+5] * b + c;
+      a[i+6] = a[i+6] * b + c;
+      a[i+7] = a[i+7] * b + c;
+      a[i] = a[i] * b + c;
+      a[i+1] = a[i+1] * b + c;
+      a[i+2] = a[i+2] * b + c;
+      a[i+3] = a[i+3] * b + c;
+      a[i+4] = a[i+4] * b + c;
+      a[i+5] = a[i+5] * b + c;
+      a[i+6] = a[i+6] * b + c;
+      a[i+7] = a[i+7] * b + c;
+      a[i] = a[i] * b + c;
+      a[i+1] = a[i+1] * b + c;
+      a[i+2] = a[i+2] * b + c;
+      a[i+3] = a[i+3] * b + c;
+      a[i+4] = a[i+4] * b + c;
+      a[i+5] = a[i+5] * b + c;
+      a[i+6] = a[i+6] * b + c;
+      a[i+7] = a[i+7] * b + c;
+//       a[i] = a[i] * b + c;
+//       a[i+1] = a[i+1] * b + c;
+//       a[i+2] = a[i+2] * b + c;
+//       a[i+3] = a[i+3] * b + c;
+//       a[i+4] = a[i+4] * b + c;
+//       a[i+5] = a[i+5] * b + c;
+//       a[i+6] = a[i+6] * b + c;
+//       a[i+7] = a[i+7] * b + c;
+//       a[i] = a[i] * b + c;
+//       a[i+1] = a[i+1] * b + c;
+//       a[i+2] = a[i+2] * b + c;
+//       a[i+3] = a[i+3] * b + c;
+//       a[i+4] = a[i+4] * b + c;
+//       a[i+5] = a[i+5] * b + c;
+//       a[i+6] = a[i+6] * b + c;
+//       a[i+7] = a[i+7] * b + c;
   }
+    
+
+//   #pragma unroll(8)
+//   #pragma simd
+//   for (int i = 0; i < N; i+=8) {
+//     for (int j = 0; j < T; j++) {
+//       a[i] = a[i] * b + c;
+//     }
+//      for (int j = 0; j < T; j++) {
+//       a[i+1] = a[i+1] * b + c;
+//     }
+//      for (int j = 0; j < T; j++) {
+//       a[i+2] = a[i+2] * b + c;
+//     }
+//      for (int j = 0; j < T; j++) {
+//       a[i+3] = a[i+3] * b + c;
+//     }
+//      for (int j = 0; j < T; j++) {
+//       a[i+4] = a[i+4] * b + c;
+//     }
+//      for (int j = 0; j < T; j++) {
+//       a[i+5] = a[i+5] * b + c;
+//     }
+//      for (int j = 0; j < T; j++) {
+//       a[i+6] = a[i+6] * b + c;
+//     }
+//      for (int j = 0; j < T; j++) {
+//       a[i+7] = a[i+7] * b + c;
+//     }
+//   }
 }
