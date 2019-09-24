02751ef18968b29643b4d37cddc4857bf6a56114
Toby Isaac <tisaac@cc.gatech.edu>
2019-09-24 14:28:53 -0400

    verlet_accelerate: added no-memory-movement version of loop

--- assignments/4-interacting-particles/verlet_accelerate.c
+++ assignments/4-interacting-particles/verlet_accelerate.c
@@ -7,6 +7,123 @@
 #else
 #define K 16
 #endif
+
+#if defined(DUMMY_ACCEL) && DUMMY_ACCEL
+void
+verlet_step_accelerate (int Np, double dt, const double *restrict X[3], double *restrict U[3])
+{
+  int i;
+  int Nplim = Np - (Np % K);
+  double x[3][K];
+  double y[3][K];
+
+  if (!Np) return;
+
+  for (int l = 0; l < K; l++) {
+    for (int d = 0; d < 3; d++) {
+      x[d][l] = X[d][0];
+      y[d][l] = X[d][0] + 0.5;
+    }
+  }
+  
+  #pragma omp parallel for schedule(static)
+  // write updates for K particles at a time
+  for (i = 0; i < Nplim; i += K) {
+    double u[3][K] = {0.};
+    double x[3][K];
+    int j;
+
+    // read the other particles in groups of K as well
+    for (j = 0; j < Nplim; j += K) {
+      double dux[K][K];
+      double duy[K][K];
+      double duz[K][K];
+      double y[3][K];
+
+      // COMPUTATION: we've set ourselves up for K^2 computations of force
+      // with all data already read into the cache
+      for (int l = 0; l < K; l++) {
+        double ddux[K];
+        double dduy[K];
+        double dduz[K];
+        force_vec (K, dt, x[0][l], x[1][l], x[2][l],
+                   &(y[0][0]), &(y[1][0]), &(y[2][0]),
+                   ddux, dduy, dduz);
+        for (int m = 0; m < K; m++) {
+          dux[l][m] = ddux[m];
+          duy[l][m] = dduy[m];
+          duz[l][m] = dduz[m];
+        }
+      }
+      // zero out the interactions of a particle with itself
+      if (i == j) {
+        for (int l = 0; l < K; l++) {
+          dux[l][l] = 0.;
+          duy[l][l] = 0.;
+          duz[l][l] = 0.;
+        }
+      }
+      // sum the contributions of this block of particles
+      for (int m = 0; m < K; m++) {
+        for (int l = 0; l < K; l++) {
+          u[0][l] += dux[l][m];
+          u[1][l] += duy[l][m];
+          u[2][l] += duz[l][m];
+        }
+      }
+    }
+    // remainder loop
+    for (j = Nplim ; j < Np; j++) {
+      double du[K][3];
+      double y[3][K];
+
+      for (int l = 0; l < K; l++) {
+        force (dt, x[0][l], x[1][l], x[2][l], y[0][l], y[1][l], y[2][l], &(du[l][0]));
+      }
+      for (int l = 0; l < K; l++) {
+        if (i + l == j) {
+          for (int d = 0; d < 3; d++) {
+            du[l][d] = 0.;
+          }
+        }
+      }
+      for (int l = 0; l < K; l++) {
+        for (int d = 0; d < 3; d++) {
+          u[d][l] += du[l][d];
+        }
+      }
+    }
+    if (!i) {
+      for (int l = 0; l < K; l++) {
+        for (int d = 0; d < 3; d++) {
+          U[d][i + l] += u[d][l];
+        }
+      }
+    }
+  }
+  // remainder loop
+  for (i = Nplim; i < Np; i++) {
+    double u[3] = {0.};
+
+    for (int j = 0; j < Np; j++) {
+      if (j != i) {
+        double du[3];
+
+        force (dt, x[0][0], x[1][0], x[2][0], y[0][0], y[1][0], y[2][0], du);
+
+        for (int d = 0; d < 3; d++) {
+          u[d] += du[d];
+        }
+      }
+    }
+    if (!i) {
+      for (int d = 0; d < 3; d++) {
+        U[d][i] += u[d];
+      }
+    }
+  }
+}
+#else
 void
 verlet_step_accelerate (int Np, double dt, const double *restrict X[3], double *restrict U[3])
 {
@@ -141,4 +258,5 @@ verlet_step_accelerate (int Np, double dt, const double *restrict X[3], double *
   }
 #endif
 }
+#endif
 
