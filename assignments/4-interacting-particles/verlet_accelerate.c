6e78c5964d62b42c02ddd6f429ef62fe4ef507ca
Toby Isaac <tisaac@cc.gatech.edu>
2019-09-24 08:45:40 -0400

    verlet_accelerate: group force computations

--- assignments/4-interacting-particles/verlet_accelerate.c
+++ assignments/4-interacting-particles/verlet_accelerate.c
@@ -2,10 +2,92 @@
 #include "verlet.h"
 #include "cloud.h"
 
+#define K 4
 void
 verlet_step_accelerate (int Np, double dt, const double *restrict X[3], double *restrict U[3])
 {
-  for (int i = 0; i < Np; i++) {
+  int Nplim = Np - (Np % K);
+  int i;
+
+#if defined(GROUP_FORCE)
+  #pragma omp parallel for schedule(static)
+  // write updates for K particles at a time
+  for (i = 0; i < Nplim; i += K) {
+    double u[3][K] = {0.};
+    double x[3][K];
+    int j;
+
+    // explicitly read the positions of the K particles into a temporary variable
+    for (int l = 0; l < K; l++) {
+      for (int d = 0; d < 3; d++) {
+        x[d][l] = X[d][i + l];
+      }
+    }
+
+    // read the other particles in groups of K as well
+    for (j = 0; j < Nplim ; j += K) {
+      double du[K][K][3];
+      double y[3][K];
+
+      // explicitly read the positions of the K particles into a temporary variable
+      for (int m = 0; m < K; m++) {
+        for (int d = 0; d < 3; d++) {
+          y[d][m] = X[d][j + m];
+        }
+      }
+      // COMPUTATION: we've set ourselves up for K^2 computations of force
+      // without all data already read into the cache
+      for (int l = 0; l < K; l++) {
+        for (int m = 0; m < K; m++) {
+          force (dt, x[0][l], x[1][l], x[2][l], y[0][m], y[1][m], y[2][m], &(du[l][m][0]));
+        }
+      }
+      // zero out the interactions of a particle with itself
+      if (i == j) {
+        for (int l = 0; l < K; l++) {
+          for (int d = 0; d < 3; d++) {
+            du[l][l][d] = 0.;
+          }
+        }
+      }
+      // sum the contributions of this block of particles
+      for (int l = 0; l < K; l++) {
+        for (int m = 0; m < K; m++) {
+          for (int d = 0; d < 3; d++) {
+            u[d][l] += du[l][m][d];
+          }
+        }
+      }
+    }
+    // remainder loop
+    for (; j < Np; j++) {
+      double du[K][3];
+      double y[3][K];
+
+      for (int l = 0; l < K; l++) {
+        force (dt, x[0][l], x[1][l], x[2][l], X[0][j], X[1][j], X[2][j], &(du[l][0]));
+      }
+      for (int l = 0; l < K; l++) {
+        if (i + l == j) {
+          for (int d = 0; d < 3; d++) {
+            du[l][d] = 0.;
+          }
+        }
+      }
+      for (int l = 0; l < K; l++) {
+        for (int d = 0; d < 3; d++) {
+          u[d][l] += du[l][d];
+        }
+      }
+    }
+    for (int l = 0; l < K; l++) {
+      for (int d = 0; d < 3; d++) {
+        U[d][i + l] += u[d][l];
+      }
+    }
+  }
+  // remainder loop
+  for (i = Nplim; i < Np; i++) {
     double u[3] = {0.};
 
     for (int j = 0; j < Np; j++) {
@@ -23,5 +105,26 @@ verlet_step_accelerate (int Np, double dt, const double *restrict X[3], double *
       U[d][i] += u[d];
     }
   }
+#else
+  #pragma omp parallel for schedule(static)
+  for (i = 0; i < Np; i++) {
+    double u[3] = {0.};
+
+    for (int j = 0; j < Np; j++) {
+      if (j != i) {
+        double du[3];
+
+        force (dt, X[0][i], X[1][i], X[2][i], X[0][j], X[1][j], X[2][j], du);
+
+        for (int d = 0; d < 3; d++) {
+          u[d] += du[d];
+        }
+      }
+    }
+    for (int d = 0; d < 3; d++) {
+      U[d][i] += u[d];
+    }
+  }
+#endif
 }
 
