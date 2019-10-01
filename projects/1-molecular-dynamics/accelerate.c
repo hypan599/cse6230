78764be0a2632d977212f1371c6c9fbbcc2eefc7
hypan599 <hanyingpan@gmail.com>
2019-10-01 14:25:45 -0400

    move force inplace

--- projects/1-molecular-dynamics/accelerate.c
+++ projects/1-molecular-dynamics/accelerate.c
@@ -6,147 +6,123 @@
 
 struct _accel_t
 {
-  int Np;
-  double L;
-  double k;
-  double r;
-  int use_ix;
-  IX ix;
+    int Np;
+    double L;
+    double k;
+    double r;
+    int use_ix;
+    IX ix;
 };
 
 int AccelCreate(int Np, double L, double k, double r, int use_ix, Accel *accel)
 {
-  int err;
-  Accel a;
+    int err;
+    Accel a;
 
-  err = safeMALLOC(sizeof(*a), &a);
-  CHK(err);
-  a->Np = Np;
-  a->L = L;
-  a->k = k;
-  a->r = r;
-  a->use_ix = use_ix;
-  if (use_ix)
-  {
-    int boxdim = 10; /* this number is magic! */
-    int maxNx;
-    maxNx = 64; /* how should we estimate the maximum number of interactions? */
-    err = IXCreate(L, boxdim, maxNx, &(a->ix));
+    err = safeMALLOC(sizeof(*a), &a);
     CHK(err);
-  }
-  else
-  {
-    a->ix = NULL;
-  }
-  *accel = a;
-  return 0;
+    a->Np = Np;
+    a->L = L;
+    a->k = k;
+    a->r = r;
+    a->use_ix = use_ix;
+    if (use_ix)
+    {
+        int boxdim = 10; /* this number is magic! */
+        int maxNx;
+        maxNx = 64; /* how should we estimate the maximum number of interactions? */
+        err = IXCreate(L, boxdim, maxNx, &(a->ix));
+        CHK(err);
+    }
+    else
+    {
+        a->ix = NULL;
+    }
+    *accel = a;
+    return 0;
 }
 
 int AccelDestroy(Accel *accel)
 {
-  int err;
+    int err;
 
-  if ((*accel)->ix)
-  {
-    err = IXDestroy(&((*accel)->ix));
-    CHK(err);
-  }
-  free(*accel);
-  *accel = NULL;
-  return 0;
+    if ((*accel)->ix)
+    {
+        err = IXDestroy(&((*accel)->ix));
+        CHK(err);
+    }
+    free(*accel);
+    *accel = NULL;
+    return 0;
 }
 
 static void
 accelerate_ix(Accel accel, Vector X, Vector U)
 {
-  IX ix = accel->ix;
-  int Np = X->Np;
-  int Npairs;
-  ix_pair *pairs;
-  double L = accel->L;
-  double k = accel->k;
-  double r = accel->r;
+    IX ix = accel->ix;
+    int Np = X->Np;
+    int Npairs;
+    ix_pair *pairs;
+    double L = accel->L;
+    double k = accel->k;
+    double r = accel->r;
 
-  for (int i = 0; i < Np; i++)
-  {
-    for (int j = 0; j < 3; j++)
+    for (int i = 0; i < Np; i++)
     {
-      IDX(U, j, i) = 0.;
+        for (int j = 0; j < 3; j++)
+        {
+            IDX(U, j, i) = 0.;
+        }
     }
-  }
 
-  //   for (int j = 0; j < 3; j++) {
-  //     for (int i = 0; i < Np; i++) {
-  //       IDX(U,j,i) = 0.;
-  //     }
-  //   }
-
-  IXGetPairs(ix, X, 2. * r, &Npairs, &pairs); // parallel in side
-#pragma omp parallel for schedule(runtime)
-  for (int p = 0; p < Npairs; p += 2)
-  {
-    int i = pairs[p].p[0];
-    int j = pairs[p].p[1];
-    double du[3];
-
-    force(k, r, L, IDX(X, 0, i), IDX(X, 1, i), IDX(X, 2, i), IDX(X, 0, j), IDX(X, 1, j), IDX(X, 2, j), du);
-    // printf("we get du: %f, %f ,%f, \n", du[0], du[1], du[2]);
-    for (int d = 0; d < 3; d++)
-    {
-#pragma omp atomic
-      IDX(U, d, i) += du[d];
-#pragma omp atomic
-      IDX(U, d, j) -= du[d];
-    }
-  }
-
-  IXRestorePairs(ix, X, 2. * r, &Npairs, &pairs);
+    IXGetPairs(ix, X, 2. * r, &Npairs, &pairs); // parallel in side
+    // IXRestorePairs(ix, X, 2. * r, &Npairs, &pairs);
 }
 
 static void
 accelerate_direct(Accel accel, Vector X, Vector U)
 {
-  int Np = accel->Np;
-  double L = accel->L;
-  double k = accel->k;
-  double r = accel->r;
+    int Np = accel->Np;
+    double L = accel->L;
+    double k = accel->k;
+    double r = accel->r;
 
 #pragma omp parallel for schedule(static, 16)
-  for (int i = 0; i < Np; i++)
-  {
-    double u[3] = {0.};
-
-    for (int j = 0; j < Np; j++)
+    for (int i = 0; i < Np; i++)
     {
-      if (j != i)
-      {
-        double du[3];
+        double u[3] = {0.};
 
-        force(k, r, L, IDX(X, 0, i), IDX(X, 1, i), IDX(X, 2, i), IDX(X, 0, j), IDX(X, 1, j), IDX(X, 2, j), du);
+        for (int j = 0; j < Np; j++)
+        {
+            if (j != i)
+            {
+                double du[3];
 
+                force(k, r, L, IDX(X, 0, i), IDX(X, 1, i), IDX(X, 2, i), IDX(X, 0, j), IDX(X, 1, j), IDX(X, 2, j), du);
+
+                for (int d = 0; d < 3; d++)
+                {
+                    u[d] += du[d];
+                }
+            }
+        }
         for (int d = 0; d < 3; d++)
         {
-          u[d] += du[d];
-        }
-      }
-    }
-    for (int d = 0; d < 3; d++)
-    {
-      /* Instead of adding to the velocity,
+            /* Instead of adding to the velocity,
        * For this project the computed interactions give the complete velocity */
-      IDX(U, d, i) = u[d];
+            IDX(U, d, i) = u[d];
+        }
     }
-  }
 }
 
 void accelerate(Accel accel, Vector X, Vector U)
 {
-  if (accel->use_ix)
-  {
-    accelerate_ix(accel, X, U);
-  }
-  else
-  {
-    accelerate_direct(accel, X, U);
-  }
+    if (accel->use_ix)
+    {
+        accelerate_ix(accel, X, U);
+    }
+    else
+    {
+        accelerate_direct(accel, X, U);
+    }
 }
--- projects/1-molecular-dynamics/interactions.c
+++ projects/1-molecular-dynamics/interactions.c
@@ -8,25 +8,25 @@
 
 typedef struct _box
 {
-  int head;
+    int head;
 } box;
 
 struct _ix
 {
-  double L;
-  double r;
-  int boxdim;
-  box ***boxes;
+    double L;
+    double r;
+    int boxdim;
+    box ***boxes;
 
 #ifdef MULTIVECTOR
-  int numContainers;
-  int *curNx;
-  int *maxNx;
-  ix_pair **pairs;
+    int numContainers;
+    int *curNx;
+    int *maxNx;
+    ix_pair **pairs;
 #else
-  int maxNx;
-  int curNx;
-  ix_pair *pairs;
+    int maxNx;
+    int curNx;
+    ix_pair *pairs;
 #endif
 };
 
@@ -51,364 +51,364 @@ int box_neighbors[NUM_BOX_NEIGHBORS][3] =
 /* maxNx: _predicted_ maximum number of interactions */
 int IXCreate(double L, int boxdim, int maxNx, IX *ix_p)
 {
-  IX ix;
-  int err;
+    IX ix;
+    int err;
 
-  if (boxdim < 4) /* need at least four boxes in each direction */
-  {
-    boxdim = 4;
-  }
-  err = safeMALLOC(sizeof(*ix), &ix);
-  CHK(err);
+    if (boxdim < 4) /* need at least four boxes in each direction */
+    {
+        boxdim = 4;
+    }
+    err = safeMALLOC(sizeof(*ix), &ix);
+    CHK(err);
 
-  ix->L = L;
-  ix->boxdim = boxdim;
-  ix->curNx = 0;
+    ix->L = L;
+    ix->boxdim = boxdim;
+    ix->curNx = 0;
 
 #ifdef MULTIVECTOR
-  ix->numContainers = omp_get_max_threads();
-  err = safeMALLOC(ix->numContainers * sizeof(int), &(ix->curNx));
-  CHK(err);
-  err = safeMALLOC(ix->numContainers * sizeof(int), &(ix->maxNx));
-  CHK(err);
-  for (int i = 0; i < ix->numContainers; i++)
-  {
-    ix->maxNx[i] = maxNx;
-    ix->curNx[i] = 0;
-  }
-  err = safeMALLOC(ix->numContainers * sizeof(ix_pair *), &(ix->pairs));
-  CHK(err);
-  for (int i = 0; i < ix->numContainers; i++)
-  {
-    err = safeMALLOC(maxNx * sizeof(ix_pair), &(ix->pairs[i]));
+    ix->numContainers = omp_get_max_threads();
+    err = safeMALLOC(ix->numContainers * sizeof(int), &(ix->curNx));
     CHK(err);
-  }
-  // printf("CREATE %d pairs finish", ix->numContainers);
-#else
-  ix->curNx = 0;
-  ix->maxNx = maxNx; // max value for each single container
-  err = safeMALLOC(maxNx * sizeof(ix_pair), &(ix->pairs));
-  CHK(err);
-#endif
-  err = safeMALLOC(boxdim * sizeof(box **), &(ix->boxes));
-  CHK(err);
-  for (int i = 0; i < boxdim; i++)
-  {
-    err = safeMALLOC(boxdim * sizeof(box *), &(ix->boxes[i]));
+    err = safeMALLOC(ix->numContainers * sizeof(int), &(ix->maxNx));
     CHK(err);
-    for (int j = 0; j < boxdim; j++)
+    for (int i = 0; i < ix->numContainers; i++)
     {
-      err = safeMALLOC(boxdim * sizeof(box), &(ix->boxes[i][j]));
-      CHK(err);
+        ix->maxNx[i] = maxNx;
+        ix->curNx[i] = 0;
     }
-  }
+    err = safeMALLOC(ix->numContainers * sizeof(ix_pair *), &(ix->pairs));
+    CHK(err);
+    for (int i = 0; i < ix->numContainers; i++)
+    {
+        err = safeMALLOC(maxNx * sizeof(ix_pair), &(ix->pairs[i]));
+        CHK(err);
+    }
+    // printf("CREATE %d pairs finish", ix->numContainers);
+#else
+    ix->curNx = 0;
+    ix->maxNx = maxNx; // max value for each single container
+    err = safeMALLOC(maxNx * sizeof(ix_pair), &(ix->pairs));
+    CHK(err);
+#endif
+    err = safeMALLOC(boxdim * sizeof(box **), &(ix->boxes));
+    CHK(err);
+    for (int i = 0; i < boxdim; i++)
+    {
+        err = safeMALLOC(boxdim * sizeof(box *), &(ix->boxes[i]));
+        CHK(err);
+        for (int j = 0; j < boxdim; j++)
+        {
+            err = safeMALLOC(boxdim * sizeof(box), &(ix->boxes[i][j]));
+            CHK(err);
+        }
+    }
 
-  *ix_p = ix;
-  return (0);
+    *ix_p = ix;
+    return (0);
 }
 
 int IXDestroy(IX *ix_p)
 {
-  int boxdim = (*ix_p)->boxdim;
+    int boxdim = (*ix_p)->boxdim;
 #ifdef MULTIVECTOR
-  free((*ix_p)->curNx);
-  free((*ix_p)->maxNx);
-  for (int i = 0; i < (*ix_p)->numContainers; i++)
-  {
-    free((*ix_p)->pairs[i]);
-  }
-#endif
-  free((*ix_p)->pairs);
-
-  for (int i = 0; i < boxdim; i++)
-  {
-    for (int j = 0; j < boxdim; j++)
+    free((*ix_p)->curNx);
+    free((*ix_p)->maxNx);
+    for (int i = 0; i < (*ix_p)->numContainers; i++)
     {
-      free((*ix_p)->boxes[i][j]);
+        free((*ix_p)->pairs[i]);
     }
-    free((*ix_p)->boxes[i]);
-  }
-  free((*ix_p)->boxes);
-  free(*ix_p);
-  *ix_p = NULL;
-  return 0;
+#endif
+    free((*ix_p)->pairs);
+
+    for (int i = 0; i < boxdim; i++)
+    {
+        for (int j = 0; j < boxdim; j++)
+        {
+            free((*ix_p)->boxes[i][j]);
+        }
+        free((*ix_p)->boxes[i]);
+    }
+    free((*ix_p)->boxes);
+    free(*ix_p);
+    *ix_p = NULL;
+    return 0;
 }
 
 static void
 IXClearPairs(IX ix)
 {
 #ifdef MULTIVECTOR
-  for (int i = 0; i < ix->numContainers; i++)
-  {
-    ix->curNx[i] = 0;
-  }
+    for (int i = 0; i < ix->numContainers; i++)
+    {
+        ix->curNx[i] = 0;
+    }
 #else
-  ix->curNx = 0;
+    ix->curNx = 0;
 #endif
 }
 
 static void
 IXPushPair(IX ix, int p1, int p2)
 {
-  ix_pair *pair;
+    ix_pair *pair;
 #ifdef MULTIVECTOR
-  int current_thread = omp_get_thread_num();
-  if (ix->curNx[current_thread] == ix->maxNx[current_thread])
-  {
-    int maxNx = ix->maxNx[current_thread] * 2;
-    ix_pair *newpairs;
+    int current_thread = omp_get_thread_num();
+    if (ix->curNx[current_thread] == ix->maxNx[current_thread])
+    {
+        int maxNx = ix->maxNx[current_thread] * 2;
+        ix_pair *newpairs;
 
-    safeMALLOC(maxNx * sizeof(ix_pair), &newpairs);
-    memcpy(newpairs, ix->pairs[current_thread], ix->curNx[current_thread] * sizeof(ix_pair));
-    free(ix->pairs[current_thread]);
-    ix->pairs[current_thread] = newpairs;
-    ix->maxNx[current_thread] = maxNx;
-  }
-  pair = &(ix->pairs[current_thread][(ix->curNx[current_thread])++]);
+        safeMALLOC(maxNx * sizeof(ix_pair), &newpairs);
+        memcpy(newpairs, ix->pairs[current_thread], ix->curNx[current_thread] * sizeof(ix_pair));
+        free(ix->pairs[current_thread]);
+        ix->pairs[current_thread] = newpairs;
+        ix->maxNx[current_thread] = maxNx;
+    }
+    pair = &(ix->pairs[current_thread][(ix->curNx[current_thread])++]);
 #else
-  if (ix->curNx == ix->maxNx)
-  {
-    int maxNx = ix->maxNx * 2;
-    ix_pair *newpairs;
+    if (ix->curNx == ix->maxNx)
+    {
+        int maxNx = ix->maxNx * 2;
+        ix_pair *newpairs;
 
-    safeMALLOC(maxNx * sizeof(ix_pair), &newpairs);
-    memcpy(newpairs, ix->pairs, ix->curNx * sizeof(ix_pair));
-    free(ix->pairs);
-    ix->pairs = newpairs;
-    ix->maxNx = maxNx;
-  }
-  pair = &(ix->pairs[ix->curNx++]);
+        safeMALLOC(maxNx * sizeof(ix_pair), &newpairs);
+        memcpy(newpairs, ix->pairs, ix->curNx * sizeof(ix_pair));
+        free(ix->pairs);
+        ix->pairs = newpairs;
+        ix->maxNx = maxNx;
+    }
+    pair = &(ix->pairs[ix->curNx++]);
 #endif
-  pair->p[0] = p1;
-  pair->p[1] = p2;
+    pair->p[0] = p1;
+    pair->p[1] = p2;
 }
 
 int interactions_check(IX ix, Vector X, double r, int Npairs, ix_pair *pairs, int *total)
 {
-  double L = ix->L;
-  double r2 = r * r;
-  int Np = X->Np;
-  int intcount = 0;
+    double L = ix->L;
+    double r2 = r * r;
+    int Np = X->Np;
+    int intcount = 0;
 
-  for (int i = 0; i < Np; i++)
-  {
-    for (int j = i + 1; j < Np; j++)
+    for (int i = 0; i < Np; i++)
     {
-      double dx, dy, dz;
+        for (int j = i + 1; j < Np; j++)
+        {
+            double dx, dy, dz;
 
-      double dist2 = dist_and_disp(IDX(X, 0, i), IDX(X, 1, i), IDX(X, 2, i),
-                                   IDX(X, 0, j), IDX(X, 1, j), IDX(X, 2, j), L,
-                                   &dx, &dy, &dz);
-      if (dist2 < r2)
-      {
-        intcount++;
-        int k;
-        for (k = 0; k < Npairs; k++)
-        {
-          if ((pairs[k].p[0] == i && pairs[k].p[1] == j) ||
-              (pairs[k].p[0] == j && pairs[k].p[1] == i))
-          {
-            break;
-          }
+            double dist2 = dist_and_disp(IDX(X, 0, i), IDX(X, 1, i), IDX(X, 2, i),
+                                         IDX(X, 0, j), IDX(X, 1, j), IDX(X, 2, j), L,
+                                         &dx, &dy, &dz);
+            if (dist2 < r2)
+            {
+                intcount++;
+                int k;
+                for (k = 0; k < Npairs; k++)
+                {
+                    if ((pairs[k].p[0] == i && pairs[k].p[1] == j) ||
+                        (pairs[k].p[0] == j && pairs[k].p[1] == i))
+                    {
+                        break;
+                    }
+                }
+                if (k == Npairs)
+                {
+                    fprintf(stderr, "Pair %d %d not in list\n", i, j);
+                    return 1;
+                }
+            }
         }
-        if (k == Npairs)
-        {
-          fprintf(stderr, "Pair %d %d not in list\n", i, j);
-          return 1;
-        }
-      }
     }
-  }
-  *total = intcount;
-  return 0;
+    *total = intcount;
+    return 0;
 }
 
 int IXGetPairs(IX ix, Vector X, double r, int *Npairs, ix_pair **pairs)
 {
-  int boxdim = ix->boxdim;
-  double L = ix->L;
-  double boxwidth = L / boxdim;
-  double cutoff2 = r * r;
-  box ***b = ix->boxes;
-  int Np = X->Np;
-  int err;
+    int boxdim = ix->boxdim;
+    double L = ix->L;
+    double boxwidth = L / boxdim;
+    double cutoff2 = r * r;
+    box ***b = ix->boxes;
+    int Np = X->Np;
+    int err;
 
-  if (r > boxwidth)
-  {
-    printf("interactions: radius %g is greater than box width %g\n", r, boxwidth);
-    return 1;
-  }
-
-  // box indices
-  int idx, idy, idz;
-  int neigh_idx, neigh_idy, neigh_idz;
-  int *next;
-  box *bp, *neigh_bp;
-
-  for (int i = 0; i < boxdim; i++)
-  {
-    for (int j = 0; j < boxdim; j++)
+    if (r > boxwidth)
     {
-      for (int k = 0; k < boxdim; k++)
-      {
-        b[i][j][k].head = -1;
-      }
+        printf("interactions: radius %g is greater than box width %g\n", r, boxwidth);
+        return 1;
     }
-  }
 
-  err = safeMALLOC(Np * sizeof(int), &next);
-  CHK(err);
+    // box indices
+    int idx, idy, idz;
+    int neigh_idx, neigh_idy, neigh_idz;
+    int *next;
+    box *bp, *neigh_bp;
 
-  // traverse all particles and assign to boxes
-  for (int i = 0; i < Np; i++)
-  {
-    double pos_p[3];
-    // initialize entry of implied linked list
-    next[i] = -1;
-
-    // get the periodic coordinates in [0,L)
-    // for (int j = 0; j < 3; j++)
-    // {
-    //   pos_p[j] = force_remainder(IDX(X, j, i), L) + L / 2.;
-    // }
-    pos_p[0] = force_remainder(IDX(X, 0, i), L) + L / 2.;
-    pos_p[1] = force_remainder(IDX(X, 1, i), L) + L / 2.;
-    pos_p[2] = force_remainder(IDX(X, 2, i), L) + L / 2.;
-    // printf("pos are: %f, %f, %f", pos_p[0], pos_p[1], pos_p[2]);
-    // which box does the particle belong to?
-    idx = (int)(pos_p[0] / L * boxdim);
-    idy = (int)(pos_p[1] / L * boxdim);
-    idz = (int)(pos_p[2] / L * boxdim);
-    // printf("idx, idy, idz is: %d, %d ,%d, \n", idx, idy, idz);
-    // add to beginn of ingimplied linked list
-    // if (pos_p[0] != pos_p[0]) {
-    //   printf("nan happened at %d with U value:%f, %f %f,\n", i, IDX(U, 0, i), IDX(U, 1, i), IDX(U, 2, i));
-    //   exit(1);
-    // }
-    bp = &b[idx][idy][idz];
-    next[i] = bp->head;
-    bp->head = i;
-  }
-
-  int p1, p2;
-  double d2, dx, dy, dz;
-
-  IXClearPairs(ix);
-#pragma omp parallel for schedule(static) private(p1, p2, idx, idy, idz)
-  for (idx = 0; idx < boxdim; idx++)
-  {
-    for (idy = 0; idy < boxdim; idy++)
+    for (int i = 0; i < boxdim; i++)
     {
-      for (idz = 0; idz < boxdim; idz++)
-      {
+        for (int j = 0; j < boxdim; j++)
+        {
+            for (int k = 0; k < boxdim; k++)
+            {
+                b[i][j][k].head = -1;
+            }
+        }
+    }
+
+    err = safeMALLOC(Np * sizeof(int), &next);
+    CHK(err);
+
+    // traverse all particles and assign to boxes
+    for (int i = 0; i < Np; i++)
+    {
+        double pos_p[3];
+        // initialize entry of implied linked list
+        next[i] = -1;
+
+        // get the periodic coordinates in [0,L)
+        // for (int j = 0; j < 3; j++)
+        // {
+        //   pos_p[j] = force_remainder(IDX(X, j, i), L) + L / 2.;
+        // }
+        pos_p[0] = force_remainder(IDX(X, 0, i), L) + L / 2.;
+        pos_p[1] = force_remainder(IDX(X, 1, i), L) + L / 2.;
+        pos_p[2] = force_remainder(IDX(X, 2, i), L) + L / 2.;
+        // printf("pos are: %f, %f, %f", pos_p[0], pos_p[1], pos_p[2]);
+        // which box does the particle belong to?
+        idx = (int)(pos_p[0] / L * boxdim);
+        idy = (int)(pos_p[1] / L * boxdim);
+        idz = (int)(pos_p[2] / L * boxdim);
+        // printf("idx, idy, idz is: %d, %d ,%d, \n", idx, idy, idz);
+        // add to beginn of ingimplied linked list
+        // if (pos_p[0] != pos_p[0]) {
+        //   printf("nan happened at %d with U value:%f, %f %f,\n", i, IDX(U, 0, i), IDX(U, 1, i), IDX(U, 2, i));
+        //   exit(1);
+        // }
         bp = &b[idx][idy][idz];
-
-        // within box interactions
-        p1 = bp->head;
-        while (p1 != -1)
-        {
-          p2 = next[p1];
-          while (p2 != -1)
-          {
-            d2 = dist_and_disp(IDX(X, 0, p1), IDX(X, 1, p1), IDX(X, 2, p1),
-                               IDX(X, 0, p2), IDX(X, 1, p2), IDX(X, 2, p2), L,
-                               &dx, &dy, &dz);
-
-            if (d2 < cutoff2)
-            {
-              IXPushPair(ix, p1, p2);
-            }
-
-            p2 = next[p2];
-          }
-          p1 = next[p1];
-        }
-
-        // interactions with other boxes
-        for (int j = 0; j < NUM_BOX_NEIGHBORS; j++)
-        {
-          neigh_idx = (idx + box_neighbors[j][0] + boxdim) % boxdim;
-          neigh_idy = (idy + box_neighbors[j][1] + boxdim) % boxdim;
-          neigh_idz = (idz + box_neighbors[j][2] + boxdim) % boxdim;
-
-          neigh_bp = &b[neigh_idx][neigh_idy][neigh_idz];
-
-          p1 = neigh_bp->head;
-          while (p1 != -1)
-          {
-            p2 = bp->head;
-            while (p2 != -1)
-            {
-              d2 = dist_and_disp(IDX(X, 0, p1), IDX(X, 1, p1), IDX(X, 2, p1),
-                                 IDX(X, 0, p2), IDX(X, 1, p2), IDX(X, 2, p2), L,
-                                 &dx, &dy, &dz);
-
-              if (d2 < cutoff2)
-              {
-                IXPushPair(ix, p1, p2);
-              }
-
-              p2 = next[p2];
-            }
-            p1 = next[p1];
-          }
-        }
-      }
+        next[i] = bp->head;
+        bp->head = i;
     }
-  }
-  // printf("GET PAIRS done, now merge results");
-  free(next);
-#ifdef MULTIVECTOR
-  // have to join result of all pairs here
-  int totalNumPairs = 0;
-  for (int i = 0; i < ix->numContainers; i++)
-  {
-    totalNumPairs += ix->curNx[i];
-  }
-  *Npairs = totalNumPairs;
-  ix_pair *totalPairs;
-  err = safeMALLOC(totalNumPairs * sizeof(ix_pair), &totalPairs);
-  CHK(err);
-  int tmp = 0;
-  for (int i = 0; i < ix->numContainers; i++)
-  {
-    for (int j = 0; j < ix->curNx[i]; j++)
+
+    int p1, p2;
+    double d2, dx, dy, dz;
+
+    IXClearPairs(ix);
+#pragma omp parallel for schedule(static) private(p1, p2, idx, idy, idz) collapse(3)
+    for (idx = 0; idx < boxdim; idx++)
     {
-      totalPairs[tmp + j] = ix->pairs[i][j];
+        for (idy = 0; idy < boxdim; idy++)
+        {
+            for (idz = 0; idz < boxdim; idz++)
+            {
+                bp = &b[idx][idy][idz];
+
+                // within box interactions
+                p1 = bp->head;
+                while (p1 != -1)
+                {
+                    p2 = next[p1];
+                    while (p2 != -1)
+                    {
+                        d2 = dist_and_disp(IDX(X, 0, p1), IDX(X, 1, p1), IDX(X, 2, p1),
+                                           IDX(X, 0, p2), IDX(X, 1, p2), IDX(X, 2, p2), L,
+                                           &dx, &dy, &dz);
+
+                        if (d2 < cutoff2)
+                        {
+                            IXPushPair(ix, p1, p2);
+                        }
+
+                        p2 = next[p2];
+                    }
+                    p1 = next[p1];
+                }
+
+                // interactions with other boxes
+                for (int j = 0; j < NUM_BOX_NEIGHBORS; j++)
+                {
+                    neigh_idx = (idx + box_neighbors[j][0] + boxdim) % boxdim;
+                    neigh_idy = (idy + box_neighbors[j][1] + boxdim) % boxdim;
+                    neigh_idz = (idz + box_neighbors[j][2] + boxdim) % boxdim;
+
+                    neigh_bp = &b[neigh_idx][neigh_idy][neigh_idz];
+
+                    p1 = neigh_bp->head;
+                    while (p1 != -1)
+                    {
+                        p2 = bp->head;
+                        while (p2 != -1)
+                        {
+                            d2 = dist_and_disp(IDX(X, 0, p1), IDX(X, 1, p1), IDX(X, 2, p1),
+                                               IDX(X, 0, p2), IDX(X, 1, p2), IDX(X, 2, p2), L,
+                                               &dx, &dy, &dz);
+
+                            if (d2 < cutoff2)
+                            {
+                                IXPushPair(ix, p1, p2);
+                            }
+
+                            p2 = next[p2];
+                        }
+                        p1 = next[p1];
+                    }
+                }
+#ifdef MULTIVECTOR
+                // all pairs get here
+                int current_thread = omp_get_thread_num();
+                for (int p = 0; p < ix->curNx[i]; p += 1)
+                {
+                    int i = ix->pairs[current_thread][p].p[0];
+                    int j = ix->pairs[current_thread][p].p[1];
+                    double du[3];
+
+                    force(k, r, L, IDX(X, 0, i), IDX(X, 1, i), IDX(X, 2, i), IDX(X, 0, j), IDX(X, 1, j), IDX(X, 2, j), du);
+                    for (int d = 0; d < 3; d++)
+                    {
+#pragma omp atomic
+                        IDX(U, d, i) += du[d];
+#pragma omp atomic
+                        IDX(U, d, j) -= du[d];
+                    }
+                }
+#endif
+            }
+        }
     }
-    tmp += ix->curNx[i];
-  }
-  // *Npairs = tmp;
-  *pairs = totalPairs;
-  // free(totalPairs);
+    // printf("GET PAIRS done, now merge results");
+    free(next);
+#ifdef MULTIVECTOR
+    // have to join result of all pairs here
+    *Npairs = 0;
+    *pairs = NULL;
 #else
-  *Npairs = ix->curNx;
-  *pairs = ix->pairs;
+    *Npairs = ix->curNx;
+    *pairs = ix->pairs;
 #endif
 
 #if DEBUG
-  {
-    int NpairsCheck;
-
-    err = interactions_check(ix, X, r, *Npairs, *pairs, &NpairsCheck);
-    CHK(err);
-
-    printf("Npairs %d, NpairsCheck %d\n", *Npairs, NpairsCheck);
-
-    if (*Npairs != NpairsCheck)
     {
-      fprintf(stderr, "Interaction count mismatch, %d != %d\n", *Npairs, NpairsCheck);
+        int NpairsCheck;
+
+        err = interactions_check(ix, X, r, *Npairs, *pairs, &NpairsCheck);
+        CHK(err);
+
+        printf("Npairs %d, NpairsCheck %d\n", *Npairs, NpairsCheck);
+
+        if (*Npairs != NpairsCheck)
+        {
+            fprintf(stderr, "Interaction count mismatch, %d != %d\n", *Npairs, NpairsCheck);
+        }
     }
-  }
 #endif
 
-  return 0;
+    return 0;
 }
 
 int IXRestorePairs(IX ix, Vector X, double r, int *Npairs, ix_pair **pairs)
 {
-  *Npairs = 0;
-  free(*pairs);
-  // *pairs = NULL;
-  return 0;
+    *Npairs = 0;
+    free(*pairs);
+    // *pairs = NULL;
+    return 0;
 }
