81c49e908da04a296d8a8e9fc4d964dd4f519add
hypan599 <hanyingpan@gmail.com>
2019-10-20 18:48:07 -0400

    fix typo

--- assignments/6-dense-matvec/dmv.c
+++ assignments/6-dense-matvec/dmv.c
@@ -74,7 +74,7 @@ int DMVCommGetRankCoordinates2D(MPI_Comm comm, int *num_rows_p, int *row_p, int 
   /* TODO: HINT, lookup MPI_Dims_create() */
   int dims[2] = {0};
   err = MPI_Dims_create(size, 2, dims);
-  print("call with size %d and dim 2 we get (%d, %d)on %d-th node\n", size, dims[0], dims[1], rank);
+  printf("call with size %d and dim 2 we get (%d, %d)on %d-th node\n", size, dims[0], dims[1], rank);
 
   *num_cols_p = num_cols;
   *num_rows_p = num_rows;
