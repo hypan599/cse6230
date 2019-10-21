aa39726a859ef6c1604196b3cc6a0c5d6df65d59
hypan599 <hanyingpan@gmail.com>
2019-10-20 21:15:49 -0400

    print params of 2d

--- assignments/6-dense-matvec/dmv_matvec_2d.c
+++ assignments/6-dense-matvec/dmv_matvec_2d.c
@@ -19,6 +19,7 @@ int DenseMatVec_2dPartition(Args args, int mStart, int mEnd, int nStart, int nEn
    * 5. Use MPI_Reduce_scatter() on the row communicator to sum all of the row contributions to vecLeftLocal.
    *      Look at DenseMatVec_ColPartition() in dmv_matvec_col.c for an example of use MPI_Reduce_scatter() in this wary, but adapt it to the row communicator.
    */
+  /* HINT
   // step1
   int numRows, row, numCols, col;
   numRows = num_cols = row = col = -1;
@@ -30,7 +31,43 @@ int DenseMatVec_2dPartition(Args args, int mStart, int mEnd, int nStart, int nEn
   err = MPI_Comm_split(comm, row, rank, &rowComm); MPI_CHK(err);
 
   // step3
-  
+  int colCommSize = 0;
+  int colCommRank = 0;
+  err = MPI_Comm_size(args->comm, &colCommSize); MPI_CHK(err);
+  err = MPI_Comm_rank(args->comm, &colCommRank); MPI_CHK(err);
+
+  int nRightLocal = rEnd - rStart;
+  temp_vec_right = (double *)malloc((mEnd - mStart) * sizeof(double));
+  if (!temp_vec_right) MPI_CHK(1);
+
+  nLocals = (int *) malloc(colCommSize * sizeof(int));
+  if (!nLocals) MPI_CHK(1);
+  nOffsets = (int *) malloc((colCommSize + 1) * sizeof(int));
+  if (!nOffsets) MPI_CHK(1);
+  // Gather the counts for every process
+  err = MPI_Allgather(&nRightLocal, 1, MPI_INT, nLocals, 1, MPI_INT, colComm); MPI_CHK(err);
+  // Turn the counts into the offsets
+  nOffsets[0] = 0;
+  for (int q = 0; q < colCommSize; q++) {
+    nOffsets[q + 1] = nOffsets[q] + nLocals[q];
+  }
+  err = MPI_Allgatherv(vecRightLocal, nRightLocal, MPI_DOUBLE, temp_vec_right, nRightLocal, nOffsets, MPI_DOUBLE, colComm);
+  MPI_CHK(err);
+
+  // step4
+  int num_cols = mEnd - mStart;
+  for (int r = 0; r < nEnd - nStart; r++){
+    double val = 0;
+    for (int c = 0; c < mEnd - mStart; c++){
+      val += matrixEntries[r * (mEnd - mStart) + c] * temp_vec_right[c];
+    }
+    vecLeftLocal[r] += val;
+  }
+  */
+ printf("mStart: %d, mEnd: %d, nStart: %d, nEnd: %d, lStart: %d, lEnd: %d, rStart: %d, rEnd: %d\n", mStart, mEnd, nStart, nEnd, lStart, lEnd, rStart, rEnd)
+
+
+
 
 
   return 0;
