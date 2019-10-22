511cd20b325146f42ab69c76239c52c49609ff36
hypan599 <hanyingpan@gmail.com>
2019-10-22 13:31:55 -0400

    fix pointer status

--- assignments/6-dense-matvec/dmv_matvec_2d.c
+++ assignments/6-dense-matvec/dmv_matvec_2d.c
@@ -106,7 +106,7 @@ int DenseMatVec_2dPartition(Args args, int mStart, int mEnd, int nStart, int nEn
   //                          int dest, int sendtag, int source, int recvtag, MPI_Comm comm,
   //                          MPI_Status *status)
   printf("I am %d and my left size is %d\n", rank, lLocal);
-  err = MPI_Sendrecv_replace(&lLocal, 1, MPI_INT, buddy_rank, 100, rank, 100, comm, status);
+  err = MPI_Sendrecv_replace(&lLocal, 1, MPI_INT, buddy_rank, 100, rank, 100, comm, &status);
   MPI_CHK(err);
   printf("I am %d and my new left size is %d\n", rank, lLocal);
   // err = MPI_Reduce_scatter(vecLeft, vecLeftLocal, lLocals, MPI_DOUBLE, MPI_SUM, rowComm);
