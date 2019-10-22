c2999d4446a556e87b904c5521ee9de4623b5bd6
hypan599 <hanyingpan@gmail.com>
2019-10-22 14:48:22 -0400

    switch buddy from to

--- assignments/6-dense-matvec/dmv_indices.h
+++ assignments/6-dense-matvec/dmv_indices.h
@@ -1,12 +1,13 @@
 int GetMyBuddyToRank(int numRows, int numCols, int row, int col)
 {
+    int curr_rank = col * numRows + row;
+    int rrow, rcol;
+    rrow = curr_rank / numCols;
+    rcol = curr_rank % numCols;
     return row * numCols + col;
 }
 int GetMyBuddyFromRank(int numRows, int numCols, int row, int col)
 {
     int curr_rank = col * numRows + row;
-    int rrow, rcol;
-    rrow = curr_rank / numCols;
-    rcol = curr_rank % numCols;
-    return rcol * numRows + rrow;
+    return row * numCols + col;
 }
