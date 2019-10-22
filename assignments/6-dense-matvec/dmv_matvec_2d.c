70ab4a97c7f533cceb3d2cb6ff52e0d0a2c7c292
hypan599 <hanyingpan@gmail.com>
2019-10-22 13:42:52 -0400

    remove unused barrier

--- assignments/6-dense-matvec/dmv_matvec_2d.c
+++ assignments/6-dense-matvec/dmv_matvec_2d.c
@@ -122,7 +122,6 @@ int DenseMatVec_2dPartition(Args args, int mStart, int mEnd, int nStart, int nEn
 
   err = MPI_Sendrecv_replace(&lLocal, 1, MPI_INT, buddy_rank, 100, buddy_rank, 100, comm, MPI_STATUS_IGNORE);
   MPI_CHK(err);
-  MPI_Barrier();
   printf("I am %d and my new left size is %d\n", rank, lLocal);
   // err = MPI_Reduce_scatter(vecLeft, vecLeftLocal, lLocals, MPI_DOUBLE, MPI_SUM, rowComm);
   // MPI_CHK(err);
