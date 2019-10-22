2fd940f2a9b0bd26724b3de3cec779ebc5f270f2
hypan599 <hanyingpan@gmail.com>
2019-10-22 14:01:29 -0400

    curr rank

--- assignments/6-dense-matvec/dmv_indices.h
+++ assignments/6-dense-matvec/dmv_indices.h
@@ -4,7 +4,7 @@ int GetMyBuddyToRank(int numRows, int numCols, int row, int col)
 }
 int GetMyBuddyFromRank(int numRows, int numCols, int row, int col)
 {
-    int curr_rank = row * numRows + col;
+    int curr_rank = col * numRows + row;
     printf("-%d-", curr_rank);
     return curr_rank / numCols + curr_rank % numCols;
 }
