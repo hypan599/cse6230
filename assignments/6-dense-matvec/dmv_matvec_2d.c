bca2b7e14bf6256907aaaa2e843941b5390dd146
hypan599 <hanyingpan@gmail.com>
2019-10-22 14:39:38 -0400

    print llocal

--- assignments/6-dense-matvec/dmv_matvec_2d.c
+++ assignments/6-dense-matvec/dmv_matvec_2d.c
@@ -102,7 +102,7 @@ int DenseMatVec_2dPartition(Args args, int mStart, int mEnd, int nStart, int nEn
   buddy_from = GetMyBuddyFromRank(numRows, numCols, row, col);
 
   // printf("I am %d and my buddy is from %d to %d\n", rank, buddy_from, buddy_to);
-  printf("I am %d and my left size is %d\n", rank, lLocal);
+  // printf("I am %d and my left size is %d\n", rank, lLocal);
   // printf("I am %d and my new left size is %d\n", rank, lLocal);
   err = MPI_Sendrecv_replace(&lLocal, 1, MPI_INT, buddy_to, 100, buddy_from, 100, comm, MPI_STATUS_IGNORE);
   MPI_CHK(err);
@@ -115,6 +115,7 @@ int DenseMatVec_2dPartition(Args args, int mStart, int mEnd, int nStart, int nEn
   err = MPI_Reduce_scatter(MPI_IN_PLACE, vecLeft, lLocals, MPI_DOUBLE, MPI_SUM, rowComm);
   MPI_CHK(err);
 
+  printf("I am %d and lLocal: %d, lEnd-Start: %d\n", rank, lLocal, lEnd - lStart);
   err = MPI_Sendrecv(vecLeft, lLocal, MPI_DOUBLE, buddy_to, 100,
                      vecLeftLocal, lEnd - lStart, MPI_DOUBLE, buddy_from, 100, comm, MPI_STATUS_IGNORE);
   MPI_CHK(err);
