80bdb578bbd401f544e6d10c502d3e48c070a04d
hypan599 <hanyingpan@gmail.com>
2019-10-23 16:23:31 -0400

    fix ;

--- assignments/6-dense-matvec/dmv_matvec_2d.c
+++ assignments/6-dense-matvec/dmv_matvec_2d.c
@@ -77,7 +77,8 @@ int DenseMatVec_2dPartition(Args args, int mStart, int mEnd, int nStart, int nEn
   err = MPI_Allgatherv(vecRightLocal, rLocal, MPI_DOUBLE, temp_vec_right, nLocals, nOffsets, MPI_DOUBLE, colComm);
   if (verbose)
   {
-    printf("I am %d-th. I gathered nLocals: ") for (i = 0; i < numRows; i++);
+    printf("I am %d-th. I gathered nLocals: ");
+    for (i = 0; i < numRows; i++)
     {
       printf("%d, ", nLocals[i]);
     }
