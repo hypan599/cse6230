45043d382ed78ad7ee088e9f40f3383a7276694f
hypan599 <hanyingpan@gmail.com>
2019-12-10 14:33:38 -0500

    add some profile

--- final/hpl_job.sh
+++ final/hpl_job.sh
@@ -17,4 +17,6 @@ rm -v $HPL_DIR/HPL.dat $HPL_DIR/"HPL-$PROFILE.out"
 cp -v $HOME/cse6230/final/HPL-$PROFILE.dat $HPL_DIR/HPL.dat
 
 # run
-mpirun -np $PROFILE xhpl > "HPL-$PROFILE.out"
+mpirun -np $PROFILE $HPL_DIR/xhpl > "HPL-$PROFILE.out"
+
+grep WR $HPL_DIR/xhpl/"HPL-$PROFILE.out" > $HOME/cse6230/final/result-$PROFILE.txt
