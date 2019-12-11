8a60cee9e5f2377ed39a479153ee2241f107ad29
hypan599 <hanyingpan@gmail.com>
2019-12-10 23:32:12 -0500

    change script

--- final/hpl_job.sh
+++ final/hpl_job.sh
@@ -6,13 +6,18 @@ module use ${CSE6230_DIR}/modulefiles
 module load cse6230
 HPL_DIR="$HOME/hpl"
 
-PROFILE="251"
+PROFILE="1"
+NP=1
 
 # prepare dat file
-rm -v $HPL_DIR/HPL.dat $HPL_DIR/HPL-"$PROFILE".out $HOME/cse6230/final/result-"$PROFILE".txt
-cp -v $HOME/cse6230/final/HPL-$PROFILE.dat $HPL_DIR/HPL.dat
+rm -vrf $HPL_DIR/$PROFILE
+rm -v $HOME/cse6230/final/result-"$PROFILE".txt
+mkdir $HPL_DIR/$PROFILE
+cp -v $HPL_DIR/xhpl $HPL_DIR/$PROFILE/xhpl
+cp -v $HOME/cse6230/final/HPL-$PROFILE.dat $HPL_DIR/$PROFILE/HPL.dat
 
 # run
-mpirun -np $PROFILE $HPL_DIR/xhpl > HPL-"$PROFILE".out
+cd $HPL_DIR/$PROFILE
+mpirun -np $NP $HPL_DIR/$PROFILE/xhpl > $HPL_DIR/$PROFILE/HPL-"$PROFILE".out
 
-grep WR $HPL_DIR/HPL-"$PROFILE".out | tee $HOME/cse6230/final/result-"$PROFILE".txt
+grep WR $HPL_DIR/$PROFILE/HPL-"$PROFILE".out | tee $HOME/cse6230/final/result-"$PROFILE".txt
